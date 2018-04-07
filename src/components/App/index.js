eb70a8225b448af0ad2f9e9ea4a3a7bfa2fbf3f6
José Manuel Lucas <josemanuel@jmlweb.es>
2018-04-07 23:51:31 +0200

    Basic structure

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+import Header from '../Header';
+
+const App = () => (
+  <div className="App">
+    <Header title="KeepCoding React Starter" />
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+);
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './index.css';
+import logo from './logo.svg';
+
+const Header = ({ title }) => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1 className="App-title">{title}</h1>
+  </header>
+);
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Header;
